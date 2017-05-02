ce935d7210e045e4869910ee05c0937182344b84
StreetCode Academy <streetcodeacademy@SCA12-StreetCodes-MacBook-Air.local>
2017-05-01 20:17:53 -0700

    Save login work

---
+++ Template/Post.swift
@@ -0,0 +1,24 @@
+//
+//  Post.swift
+//  Template
+//
+//  Created by StreetCode Academy on 4/22/17.
+//  Copyright © 2017 StreetCode. All rights reserved.
+//
+
+import Foundation
+class Post{
+    var id : String?
+    var mics: Int?
+    var imageurl:String?
+    var videourl:String?
+    init(id:String,dictionary:AnyObject) {
+        self.id = id
+        self.mics = dictionary.object(forKey:"mics")as? Int
+        self.imageurl = dictionary.object(forKey:"imageurl")as? String
+        self.videourl = dictionary.object(forKey:"videourl")as? String
+        
+
+    }
+    
+}
